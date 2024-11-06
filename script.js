2892e1cede94650c8306cfd2cfca21f0e5dfa125
Cappybara0720 <135939380+Cappybara0720@users.noreply.github.com>
2024-11-06 16:06:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const quotes = [
-    { text: "Chúc rằng anh sẽ sớm chinh phục những vùng đất mơ ước, để lại dấu ấn trên bản đồ khám phá của mình. 🎉✨", author: "Giang Nguyễn" },
-    { text: "Mong anh tràn đầy năng lượng và cảm hứng khám phá điều mới mẻ trong sự nghiệp và cuộc sống!", author: "Hưng Nguyễn" },
-    { text: "Ước iem Chấy một đời an yên.", author: "Chí Nguyễn" },
-    { text: "Chúc lấy vợ thành công, chúc Công bớt mỏ hỗn", author: "Công Nguyễn" }
+    { text: "Chúc rằng anh sẽ sớm chinh phục những vùng đất mơ ước, để lại dấu ấn trên bản đồ khám phá của mình. 🎉✨", author: "for GiangND" },
+    { text: "Mong anh tràn đầy năng lượng và cảm hứng khám phá điều mới mẻ trong sự nghiệp và cuộc sống!", author: "for HungNV" },
+    { text: "Ước iem Chấy một đời an yên.", author: "for ChiNH" },
+    { text: "Chúc lấy vợ thành công, chúc Công bớt mỏ hỗn", author: "CôngNT" }
 ];
 
 function changeQuote() {
